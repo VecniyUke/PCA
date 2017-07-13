8a8a696643334f052c086f1723413117b5724d87
sasha <johndoe@example.com>
2017-07-13 19:06:12 +0300

    something

--- index.php
+++ index.php
@@ -2,6 +2,24 @@
  <?php include 'template/header.php'; ?>
 <img class="main-page-img" src="http://momsdish.com/sites/default/files/styles/recipe_image_1600x900_scale/public/recipe/image/1375139332.JPG" alt="">
 
- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo corporis labore cumque nisi, id. Neque ipsam officiis ratione adipisci vitae, quis fugit laboriosam. Aperiam necessitatibus sed, sunt. Officia, reprehenderit, iusto.
+<?php
 
+$names = array('Tom', 'Jack','Lily', 'Bill', 'Ivan');
+
+// var_dump($names);
+
+// foreach ($names as $key => $value) {
+//   echo $value."<br>";
+// }
+
+foreach ($names as $key => $value) {
+  echo $value."<br>";
+}
+
+
+
+
+// echo $names[0].' '.$names[1].' '.$names[3];
+
+?>
  <?php include 'template/footer.php'; ?>
